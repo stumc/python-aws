505e44ca988abcd56177d6cee5b0b9f471f101e8
StuMc <stumc68@gmail.com>
2023-10-25 19:31:44 -0400

    Fixed up lambda comments added package and unittest commands

--- lambdas/applications/GetUserConfig/src/main/python/lambda_entry.py
+++ lambdas/applications/GetUserConfig/src/main/python/lambda_entry.py
@@ -1,4 +1,6 @@
+"""Get User Preference lambda"""
 from stumc_core_library.user_prefs.dao import get_prefs
 
 def lambda_function(event, context):
+    """Entry point for lamnda"""
     return get_prefs(event)
